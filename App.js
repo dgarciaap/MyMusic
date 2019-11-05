ce3bd1bc0fb029e6d090ef773c75d5ba16c52f2a
dgarciaap <dgarcia24@ucol.mx>
2019-11-05 09:54:08 -0600

    LoginView

--- App.js
+++ App.js
@@ -7,69 +7,20 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, Switch, TextInput, Alert, Image} from 'react-native';
+import LoginView from './src/LoginView';
+import HomeView from './src/HomeView';
+import { Actions, Scene, Router } from 'react-native-router-flux';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const scenes = Actions.create(
+  <Scene key="root">
+    <Scene key="login" component={LoginView} hideNavBar />
+    <Scene key="home" component={HomeView} hideNavBar />
+  </Scene>
+)
 
 type Props = {};
 export default class App extends Component<Props> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      switchValue: false,
-      username: 'Username',
-      password: 'Password'
-    };
-  }
-
-  onChangeText(key, value) {
-    this.setState({
-      [key]: value
-    })
-  }
-
-  printMessages = () => {
-    console.warn(this.state.username)
-    console.warn(this.state.password)
-  }
-
   render() {
-    return (
-      <View style={styles.container}>
-        <Image
-          style={{width: 100, height: 100, margin: 20}}
-          source={{url: 'https://facebook.github.io/react/logo-og.png'}}
-        />
-        <TextInput
-          style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1, margin: 10 }}
-          onChangeText={text => this.onChangeText('username', text)}
-          value={this.state.username}
-        />
-        <TextInput
-          style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1, margin: 10 }}
-          onChangeText={text => this.onChangeText('password', text)}
-          value={this.state.password}
-          secureTextEntry
-        />
-        <Button
-          title="Log in"
-          onPress={this.printMessages}
-        />
-      </View>
-    );
+    return <Router scenes={scenes}/>
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  }
-});
